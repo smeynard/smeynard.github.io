8012f5bc521b1bacfab4b69bc8727bf2af058215
shelbi <smeyna1@lsu.edu>
2018-03-02 20:37:04 -0600

    update

---
+++ js/code3.js
@@ -0,0 +1,13 @@
+let Id = 1
+let Url = 'http://arcg.is/2DsHgpx'
+let Title = 'Points of Interest - Lafitte, LA'
+let Desc = 'Book Assignment 1'
+let Thumb = true
+let KeyWords = ['Lafite','Story Map']
+
+console.log('ID: ' + Id)
+console.log('URL: ' + Url)
+console.log('Title: ' + Title)
+console.log('Description: ' + Desc)
+console.log('Has thumbnail? ' + Thumb)
+console.log('KeyWords: ' + KeyWords)
